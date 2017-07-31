8dc3153ed14b24b170f8e813d98ff870990ead74
Alexander Soen <u5642715@anu.edu.au>
2017-07-31 18:13:05 +1000

    Change tableau definition

--- CPL/c_tableau_calculus.v
+++ CPL/c_tableau_calculus.v
@@ -4,21 +4,16 @@ Set Implicit Arguments.
 Module Type tableau_calculus_mod (X : base_mod).
 Import X.
 
-Inductive Tableau : Type :=
-  | T : list PropF -> Tableau.
+Definition Tableau : Type := list PropF.
 
 Inductive ClosedT_P : Tableau -> Prop :=
-  | TBot   : forall L         , In ⊥ L                               -> ClosedT_P (T L)
-  | TId    : forall L1 L2 L3 A, ClosedT_P (T (L1++A::L2++(A^c)::L3)) -> ClosedT_P (T (⊥::nil))
-  | TAnd   : forall L1 L2 A B , ClosedT_P (T (L1++A∧B::L2))          -> ClosedT_P (T (L1++A::B::L2))
-  | TOrL   : forall L1 L2 A B , ClosedT_P (T (L1++A∨B::L2))          -> ClosedT_P (T (L1++A::L2))
-  | TOrR   : forall L1 L2 A B , ClosedT_P (T (L1++A∨B::L2))          -> ClosedT_P (T (L1++B::L2))
-  | TImpL  : forall L1 L2 A B , ClosedT_P (T (L1++A→B::L2))          -> ClosedT_P (T (L1++¬A::L2))
-  | TImpR  : forall L1 L2 A B , ClosedT_P (T (L1++A→B::L2))          -> ClosedT_P (T (L1++B::L2))
-  | TNAndL : forall L1 L2 A B , ClosedT_P (T (L1++¬(A∧B)::L2))       -> ClosedT_P (T (L1++¬A::L2))
-  | TNAndR : forall L1 L2 A B , ClosedT_P (T (L1++¬(A∧B)::L2))       -> ClosedT_P (T (L1++¬B::L2))
-  | TNOr   : forall L1 L2 A B , ClosedT_P (T (L1++¬(A∨B)::L2))       -> ClosedT_P (T (L1++¬A::¬B::L2))
-  | TNImp  : forall L1 L2 A B , ClosedT_P (T (L1++¬(A→B)::L2))       -> ClosedT_P (T (L1++A::¬B::L2))
+  | TId    : forall L A       , In A L -> In (¬A) L                  -> ClosedT_P L
+  | TAnd   : forall L1 L2 A B , ClosedT_P (L1++A::B::L2) -> ClosedT_P (L1++A∧B::L2)
+  | TOr    : forall L1 L2 A B , ClosedT_P (L1++A::L2) -> ClosedT_P (L1++B::L2) -> ClosedT_P (L1++A∨B::L2)
+  | TImp   : forall L1 L2 A B , ClosedT_P (L1++¬A::L2) -> ClosedT_P (L1++B::L2) -> ClosedT_P (L1++A→B::L2)
+  | TNAnd  : forall L1 L2 A B , ClosedT_P (L1++¬A::L2) -> ClosedT_P (L1++¬B::L2) -> ClosedT_P (L1++¬(A∧B)::L2)
+  | TNOr   : forall L1 L2 A B , ClosedT_P (L1++¬A::¬B::L2) -> ClosedT_P (L1++¬(A∨B)::L2)
+  | TNImp  : forall L1 L2 A B , ClosedT_P (L1++A::¬B::L2) -> ClosedT_P (L1++¬(A→B)::L2)
 .
 
 End tableau_calculus_mod.
